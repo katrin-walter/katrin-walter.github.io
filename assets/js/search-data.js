d236f1e8a3a9fa37c24b67eac3d3915e398ebfe5
katrin-walter <katrin.walter89@gmail.com>
2026-04-30 14:45:38 +0000

    Deploying to gh-pages from @ katrin-walter/katrin-walter.github.io@0e5f66ba098bcafb600dbc8846d51382997b6f5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-activities",
+          title: "activities",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/activities/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
