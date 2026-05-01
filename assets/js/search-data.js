c954a2e7bbdf22b219a5ffe90f8f780dbcc810e4
katrin-walter <katrin.walter89@gmail.com>
2026-05-01 12:38:35 +0000

    Deploying to gh-pages from @ katrin-walter/katrin-walter.github.io@f03143f5c808898d82b9ea66d6c1cb8d459cb398 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,9 +409,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-wet-lab-end-to-end-wgs-pipeline",
+          title: 'wet lab - End-to-End WGS Pipeline',
+          description: "From Sample Extraction to Variant Analysis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
