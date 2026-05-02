ad37236294217f1bb792627b7a28faf06329bae1
katrin-walter <katrin.walter89@gmail.com>
2026-05-02 10:35:46 +0000

    Deploying to gh-pages from @ katrin-walter/katrin-walter.github.io@cb41d5f9e379c7ec0a3d1026f5ba06e9fff3f83b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-katrin-walter",
+    title: "Katrin Walter",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
